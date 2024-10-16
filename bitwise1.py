8a3062e72d9cd04b60fc0ac790ea9e846c190a22
decastrojeric <jericgithub@gmail.com>
2024-10-16 09:04:37 +0800

    Create bitwise1.py

---
+++ bitwise1.py
@@ -0,0 +1,12 @@
+char1, char2 = input("Enter two space-separated characters: ").split()
+
+greater_char = max(char1, char2)
+
+print("----------------------------")
+print(f"The character with greater value is: {greater_char}")
+print("----------------------------")
+
+
+print(f"Showing ASCII Values:")
+print(f"{char1} : {ord(char1)}")
+print(f"{char2} : {ord(char2)}")
